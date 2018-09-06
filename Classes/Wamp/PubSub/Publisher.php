b6d7c9bee8009a4d4d2d3a6175271673a3380563
Benno Weinzierl <bj@format-d.com>
2018-09-06 15:41:58 +0200

    [FEATURE] Added a publisher service for publishing to topics from flow

---
+++ Classes/Wamp/PubSub/Publisher.php
@@ -0,0 +1,68 @@
+<?php
+namespace FormatD\WampRouter\Wamp\PubSub;
+
+use Neos\Flow\Annotations as Flow;
+use FormatD\WampRouter\Annotations as WAMP;
+
+/**
+ * Service for publishing data to a topic
+ *
+ * @Flow\Scope("singleton")
+ */
+class Publisher {
+
+	/**
+	 * @param string $realm
+	 * @param string $topic
+	 * @param array $args
+	 * @param \Thruway\Authentication\ClientAuthenticationInterface $authenticator
+	 * @param string $address (IP and Port of the WampRouter)
+	 * @return string
+	 */
+	public function publish($realm, $topic, $args, $authenticator = NULL, $address = '127.0.0.1:8080') {
+
+		// workaound to prevent thruway to output to the browser
+		\Thruway\Logging\Logger::set(new \Psr\Log\NullLogger());
+
+		$result     = null;
+		$connection = new \Thruway\Connection([
+			'realm' => $realm,
+			'url'   => 'ws://' . $address,
+		]);
+
+		$connection->getClient()->setAttemptRetry(false);
+
+		if ($authenticator) {
+			$connection->getClient()->addClientAuthenticator($authenticator);
+		}
+
+		$connection->on('open', function (\Thruway\ClientSession $session) use ($connection, $args, $topic, &$result) {
+			//publish an event
+			$session->publish($topic, [$args], [], ["acknowledge" => true])->then(
+				function () use ($connection, &$result) {
+					$result = "published";
+					$connection->close(); // close connection after publish
+				},
+				function ($error) use ($connection, &$result) {
+					// publish failed
+					$result = $error;
+					$connection->close();
+				}
+			);
+		});
+
+		$connection->on('error', function ($errorCode, $message) use ($topic) {
+			throw new \FormatD\WampRouter\Exception('Error during publish to topic "' . $topic . '": ' . $errorCode);
+		});
+
+		$connection->open();
+
+		if ($result !== 'published') {
+			throw new \FormatD\WampRouter\Exception('Error during publish to topic "' . $topic . '": ' . $result);
+		}
+
+		return $result;
+	}
+
+}
+?>

d21bebe8018b3264e5363a2f97fb001f5dd07776
Benno Weinzierl <bj@format-d.com>
2018-10-23 17:58:24 +0200

    Put reflection stuff in a static compiled method

--- Classes/Wamp/Rpc/RpcClient.php
+++ Classes/Wamp/Rpc/RpcClient.php
@@ -2,6 +2,7 @@
 namespace FormatD\WampRouter\Wamp\Rpc;
 
 use Neos\Flow\Annotations as Flow;
+use \FormatD\WampRouter\Annotations\RpCallable;
 
 /**
  * Internal client providing rpc endpoints for annotated methods in flow
@@ -12,15 +13,28 @@ class RpcClient extends \Thruway\Peer\Client {
 
 	/**
 	 * @Flow\Inject
+	 * @var \Neos\Flow\ObjectManagement\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @Flow\Inject
 	 * @var \Neos\Flow\Reflection\ReflectionService
 	 */
 	protected $reflectionService;
 
 	/**
-	 * @Flow\Inject
-	 * @var \Neos\Flow\ObjectManagement\ObjectManagerInterface
+	 * @var array
 	 */
-	protected $objectManager;
+	protected $rpcMethods = [];
+
+	/**
+	 * Detect remote callable methods by annotation
+	 */
+	public function initializeObject()
+	{
+		$this->rpcMethods = static::getRpcMethods($this->objectManager);
+	}
 
     /**
      * @param \Thruway\ClientSession $session
@@ -30,15 +44,14 @@ class RpcClient extends \Thruway\Peer\Client {
 	{
         \Thruway\Logging\Logger::info($this, 'Generic RPC Client registered successfully');
 
-		$rpcClasses = $this->reflectionService->getClassesContainingMethodsAnnotatedWith(\FormatD\WampRouter\Annotations\RpCallable::class);
+		$rpcMethods = $this->rpcMethods;
 
-		foreach ($rpcClasses as $className) {
+		foreach ($rpcMethods as $className => $methods) {
 			$class = $this->objectManager->get($className);
-			$methods = $this->reflectionService->getMethodsAnnotatedWith($className, \FormatD\WampRouter\Annotations\RpCallable::class);
 
 			foreach ($methods as $methodName) {
 				/** @var \FormatD\WampRouter\Annotations\RpCallable $annotation */
-				$annotation = $this->reflectionService->getMethodAnnotation($className, $methodName, \FormatD\WampRouter\Annotations\RpCallable::class);
+				$annotation = $this->reflectionService->getMethodAnnotation($className, $methodName, RpCallable::class);
 
 				if ($annotation->getRealms() === NULL || array_search($this->getRealm(), $annotation->getRealms()) !== FALSE) {
 					\Thruway\Logging\Logger::info($this, 'Registering new RPC Endpoint: ' . $annotation->getUri());
@@ -47,5 +60,25 @@ class RpcClient extends \Thruway\Peer\Client {
 			}
 		}
     }
+
+	/**
+	 * Returns all class names and methods that are rpc callable
+	 *
+	 * @Flow\CompileStatic
+	 * @param \Neos\Flow\ObjectManagement\ObjectManagerInterface $objectManager
+	 * @return array Array of classes with methods containing a RpCallable method
+	 */
+	protected static function getRpcMethods(\Neos\Flow\ObjectManagement\ObjectManagerInterface $objectManager)
+	{
+		$reflectionService = $objectManager->get(\Neos\Flow\Reflection\ReflectionService::class);
+		$classNames = $reflectionService->getClassesContainingMethodsAnnotatedWith(RpCallable::class);
+
+		$methods = [];
+		foreach($classNames as $className) {
+			$methods[$className] = $reflectionService->getMethodsAnnotatedWith($className, RpCallable::class);
+		}
+
+		return $methods;
+	}
 
 }
